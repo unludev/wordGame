577bff566b778d46cbe558d2f8136746fa8bff97
unlu <corepavlion@gmail.com>
2022-10-21 21:06:24 +0300

    init commit

---
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameFragment.kt
@@ -0,0 +1,133 @@
+/*
+ * Copyright (C) 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and 
+ * limitations under the License.
+ */
+
+package com.example.android.unscramble.ui.game
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.example.android.unscramble.R
+import com.example.android.unscramble.databinding.GameFragmentBinding
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+
+
+class GameFragment : Fragment() {
+    /**
+     * Kotlin'deki property delegasyonu, getter-setter sorumluluğunu farklı bir sınıfa devretmenize yardımcı olur.
+     * Bu sınıf (delege sınıfı olarak adlandırılır), propertynin getter ve setter işlevlerini sağlar
+     * ve değişikliklerini handle eder..
+     * Bir delegated property, by keywordu ve bir delegate sınıfı instance i kullanılarak tanımlanır:
+     * */
+    private val viewModel: GameViewModel by viewModels()
+    //viewModel nesnesinin sorumlulugunu viewModels() classina devrettik.
+    //bu nesneye eristigimizde viewModels sinifi configuration changesleri handle edecek
+
+    private lateinit var binding: GameFragmentBinding
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = GameFragmentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.submit.setOnClickListener { onSubmitWord() }
+        binding.skip.setOnClickListener { onSkipWord() }
+        updateNextWordOnScreen()
+        binding.score.text = getString(R.string.score, 0)
+        binding.wordCount.text = getString(
+            R.string.word_count, 0, MAX_NO_OF_WORDS
+        )
+    }
+
+    private fun onSubmitWord() {
+        val playerWord = binding.textInputEditText.text.toString()
+        if (viewModel.isUserWordCorrect(playerWord)) {
+            setErrorTextField(false)
+
+            if (viewModel.nextWord()) {
+                updateNextWordOnScreen()
+            } else {
+                showFinalScoreDialog()
+            }
+        } else {
+            setErrorTextField(true)
+        }
+    }
+
+
+    private fun onSkipWord() {
+
+    }
+
+    private fun restartGame() {
+        setErrorTextField(false)
+        updateNextWordOnScreen()
+    }
+
+    /*
+     * Exits the game.
+     */
+    private fun exitGame() {
+        activity?.finish()
+    }
+
+    /*
+    * Sets and resets the text field error status.
+    */
+    private fun setErrorTextField(error: Boolean) {
+        with(binding) {
+            if (error) {
+                textField.isErrorEnabled = true
+                textField.error = getString(R.string.try_again)
+            } else {
+                textField.isErrorEnabled = false
+                textInputEditText.text = null
+            }
+        }
+
+    }
+
+    /*
+     * Displays the next scrambled word on screen.
+     */
+    private fun updateNextWordOnScreen() {
+        binding.textViewUnscrambledWord.text = viewModel.currentScrambledWord
+    }
+
+    //show dialog for final screen
+    private fun showFinalScoreDialog() {
+        MaterialAlertDialogBuilder(requireContext())
+            .setTitle(getString(R.string.congratulations))
+            .setMessage(getString(R.string.you_scored, viewModel.score))
+            .setCancelable(false)
+            .setNegativeButton(getString(R.string.exit)) { _, _ ->
+                exitGame()
+            }
+            .setPositiveButton(getString(R.string.play_again)) { _, _ ->
+                restartGame()
+            }
+            .show()
+    }
+}
---
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -0,0 +1,61 @@
+package com.example.android.unscramble.ui.game
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+
+class GameViewModel: ViewModel() {
+    private var wordsList: MutableList<String> = mutableListOf()
+    private lateinit var currentWord: String
+    private var _score = 0
+    val score: Int
+        get() = _score
+
+    private var _currentWordCount = 0
+    val currentWordCount: Int
+        get() = _currentWordCount
+    private lateinit var _currentScrambledWord: String
+    val currentScrambledWord: String
+        get() = _currentScrambledWord
+
+    init {
+        Log.d("GameFragment", "GameViewModel created!")
+        getNextWord()
+    }
+
+
+    private fun getNextWord(){
+        currentWord = allWordsList.random()
+        val tempWord = currentWord.toCharArray()
+        tempWord.shuffle()
+
+        while (tempWord.toString() == currentWord) {
+            tempWord.shuffle()
+        }
+        if (wordsList.contains(currentWord)) {
+            getNextWord()
+        } else {
+            _currentScrambledWord = tempWord.toString()
+            ++_currentWordCount
+            wordsList.add(currentWord)
+        }
+    }
+
+    fun nextWord(): Boolean {
+        return if (currentWordCount < MAX_NO_OF_WORDS) {
+            getNextWord()
+            true
+        } else false
+    }
+
+    fun increaseScore(){
+        _score += SCORE_INCREASE
+    }
+
+    fun isUserWordCorrect(playerWord: String): Boolean {
+        if (playerWord == currentWord) {
+            increaseScore()
+            return true
+        }
+        return false
+    }
+}
